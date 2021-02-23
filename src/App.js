f86362e75f643a38b24d4006d8ec550170d35dbf
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-23 20:10:58 +0530

    ButtonGroup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton';
+import IconButton from '@material-ui/core/IconButton'
 import PhotoCamera from '@material-ui/icons/PhotoCamera'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button startIcon={<PhotoCamera />} href="https://google.com" target="_blank" variant="contained" color="primary">Hello World</Button>
-        <IconButton>
-          <PhotoCamera />
-        </IconButton>
+        <ButtonGroup color="secondary">
+          <Button
+            startIcon={<SaveIcon />}
+            size="large"
+            variant="contained"
+            color="primary"
+          >
+            Save
+            </Button>
+            <Button
+            startIcon={<DeleteIcon />}
+            size="large"
+            variant="contained"
+          >
+            Discard
+            </Button>
+          
+        </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
         
       </header>
